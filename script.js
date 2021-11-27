412398342c605e7d9bd51b0af4e24f8c2d6a2bd5
Vitaly Zakatov <answer-0885@mail.ru>
2021-11-27 15:16:00 +0700

    создал переменные,добавил alert и вывод в консоль

---
+++ script.js
@@ -0,0 +1,10 @@
+let title
+let screens
+let screenPrice
+let rollback
+let fullPrice
+let adaptive
+
+alert('Здесь любое сообщение в модальном окне');
+
+console.log('Сообщение в консоль с любым текстом');
