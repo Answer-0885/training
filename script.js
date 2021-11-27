4ab3405beff9992a65b3c10e9f5c3f8ef3869686
Vitaly Zakatov <answer-0885@mail.ru>
2021-11-27 20:03:41 +0700

    присвоил значения переменным и вывел их в консоль

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let title
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
+let title = 'Мой первый проект на JavaScript'
+let screens = "Простые, Сложные, Интерактивные"
+let screenPrice = 22
+let rollback = 9
+let fullPrice = 1000000
+let adaptive = true
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice);
+console.log(fullPrice);
+console.log(screens.toLowerCase());
+console.log(screens.split(','));
+console.log(fullPrice * (rollback / 100));
+
+
 
 alert('Здесь любое сообщение в модальном окне');
 
