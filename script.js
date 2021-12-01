136868d365fb624d7acf97fd997525cc0f22515d
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-01 20:25:25 +0700

    Откоректировал весь файл

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
 let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные ')
-let screenPrice = +prompt('Сколько будет стоить данная работа ?', '15000')
-let adaptive = confirm('Нужен ли адаптив на сайте?')
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные ');
+let screenPrice = +prompt('Сколько будет стоить данная работа ?', '15000');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
 
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'service1')
-let servicePrice1 = +prompt('Сколько это будет стоить?', '5000')
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'service2')
-let servicePrice2 = +prompt('Сколько это будет стоить?', '6000')
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-let rollback = 10
-let allServicePrices
-let fullPrice
-let servicePercentPrice
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'service1');
+let servicePrice1 = +prompt('Сколько это будет стоить?', '5000');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'service2');
+let servicePrice2 = +prompt('Сколько это будет стоить?', '6000');
 
 // Функция возвращает сумму всех дополнительных услуг.
-const getAllServicePrices = function (sp1, sp2) {
-   return sp1 + sp2
-}
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2)
-
+const getAllServicePrices = function () {
+   return servicePrice1 + servicePrice2
+};
 
 const showTypeOf = function (variable) {
    console.log(variable, typeof variable);
-}
-
+};
 
 //Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
-function getFullPrice(scrPr, allScrPr) {
-   return scrPr + allScrPr
-}
-fullPrice = getFullPrice(screenPrice, allServicePrices)
-
+const getFullPrice = function () {
+   return screenPrice + allServicePrices
+};
 
 //Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой". Учесть вариант что строка может начинаться с пустых символов. " КаЛьКулятор Верстки"
-let getTitle = function (title) {
-   title = title.trim().toLowerCase();
-   return title[0].toUpperCase() + title.substring(1);
-}
-getTitle(title);
+let getTitle = function () {
+   return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase();
+};
+
 
 // Функция возвращает итоговую стоимость за вычетом процента отката
-function getServicePercentPrices(fullPrice, rollback) {
+const getServicePercentPrices = function () {
    return fullPrice - (fullPrice * (rollback / 100))
-}
-servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+};
+
 
 
 
@@ -57,12 +51,26 @@ const getRollbackMessage = function (price) {
    } else {
       console.log('Что то пошло не так');
    };
-}
+};
 
-showTypeOf(title)
-showTypeOf(screenPrice)
-showTypeOf(adaptive)
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(servicePercentPrice);
 
 console.log(screens.split(','));
-console.log(getRollbackMessage(fullPrice));
-console.log(servicePercentPrice);
+
+console.log("Стоимость вёрстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
