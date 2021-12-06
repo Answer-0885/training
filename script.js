015cdd1dc5994316264436193e923605f13dfc31
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-06 18:20:42 +0700

    ДЗ по lesson08

--- script.js
+++ script.js
@@ -2,62 +2,107 @@
 
 const appData = {
    title: '',
-   screens: '',
+   screens: [],
    screenPrice: 0,
    adaptive: true,
    rollback: 10,
    allServicePrices: 0,
    fullPrice: 0,
    servicePercentPrice: 0,
-   service1: '',
-   service2: '',
-   asking: function () {
-      appData.title = prompt('Как называется ваш проект?', 'Калькулятор вёрстки');
-      appData.screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные ');
-      // цикл do while выведет окно с вопросомю
-
-      do {
-         appData.screenPrice = +prompt('Сколько будет стоить данная работа ?');
-      } while (!appData.isNumber(appData.screenPrice)) //знак отрицания переварачивает isNumber т.е. если введено не действительное число, то вернётся false и цикл продолжится по кругу, пока не получит true.
-
-      appData.adaptive = confirm('Нужен ли адаптив на сайте?');
+   services: {},
+   start: function () {
+      appData.asking();
+      appData.addPrices();
+      appData.getFullPrice();
+      appData.getServicePercentPrices();
+      appData.getTitle();
+      appData.logger();
    },
    isNumber: function (num) {
       return !isNaN(parseFloat(num)) && isFinite(num) // лучшая проверка на число. Проверяет введено ли не действительное число, если да то возвращает true.
    },
-   // Отвечает за название и стоимость дополнительных услуг.
-   getAllServicePrices: function () {
-      let sum = 0
-      for (let i = 0; i < 2; i++) {
-         let price = 0;
+   asking: function () {
 
-         if (i === 0) {
-            appData.service1 = prompt('Какой дополнительный тип услуги нужен?', 'Метрика')
-         } else if (i === 1) {
-            appData.service2 = prompt('Какой дополнительный тип услуги нужен?', 'Отправка форм');
-         }
+      appData.title = prompt('Как называется ваш проект?', 'Калькулятор вёрстки');
 
-         do {
-            price = prompt('Сколько это будет стоить?');
-         }
-         while (!appData.isNumber(price));
-         sum += +price
-
+      //проверка, на строку.
+      if (!isNaN(appData.title)) {
+         alert('Необходимо ввести строчное значение');
+         appData.asking();
       }
-      return sum;
+
+      function circleScreens() {
+         for (let i = 0; i < 2; i++) {
+            let name = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные ')
+            let price = 0
+
+            //проверка на строку
+            if (!isNaN(name)) {
+               alert('Необходимо ввести строчное значение');
+               circle()
+            }
+
+            // + делает проверку на число
+            do {
+               price = +prompt('Сколько будет стоить данная работа ?');
+            } while (!appData.isNumber(price))
+
+            appData.screens.push({
+               id: i,
+               name: name,
+               price: price
+            })
+         }
+      }
+      circleScreens();
+
+      function circleService() {
+         for (let i = 0; i < 2; i++) {
+            let name = prompt('Какой дополнительный тип услуги нужен?');
+            let price = 0;
+
+            //проверка на строку
+            if (!isNaN(name)) {
+               alert('Необходимо ввести строчное значение');
+               circleService();
+            };
+
+            do {
+               price = prompt('Сколько это будет стоить?');
+            }
+            while (!appData.isNumber(price));
+            appData.services[name] = +price;
+         }
+      }
+      circleService()
+
+      appData.adaptive = confirm('Нужен ли адаптив на сайте?');
    },
+
+   addPrices: function () {
+      for (let screen of appData.screens) { // цикл расчёта стоимости экранов
+         appData.screenPrice += +screen.price
+      };
+      for (let key in appData.services) {
+         appData.allServicePrices += appData.services[key]
+      };
+   },
+
    //Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
    getFullPrice: function () {
-      return +appData.screenPrice + appData.allServicePrices
+      appData.fullPrice = +appData.screenPrice + appData.allServicePrices
    },
+
    // Функция возвращает итоговую стоимость за вычетом процента отката
    getServicePercentPrices: function () {
-      return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
+      appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
    },
+
    //Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой". Учесть вариант что строка может начинаться с пустых символов. " КаЛьКулятор Верстки"
    getTitle: function () {
-      return appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase();
+      appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase();
    },
+
    getRollbackMessage: function (price) {
       if (price >= 30000) {
          return 'Даем скидку в 10 %';
@@ -69,25 +114,12 @@ const appData = {
          console.log('Что то пошло не так');
       };
    },
+
    logger: function () {
-      console.log('вся необходимая информация');
-
-      for (let key in appData) {
-         console.log('ключ ' + key + ' значение ' + appData[key]);
-      };
+      console.log(appData.fullPrice);
+      console.log(appData.servicePercentPrice);
+      console.log(appData.screens);
    },
-   start: function () {
-      appData.asking();
-      appData.allServicePrices = appData.getAllServicePrices();
-      appData.fullPrice = appData.getFullPrice();
-      appData.servicePercentPrice = appData.getServicePercentPrices();
-      appData.title = appData.getTitle();
-      appData.logger();
-   }
-
 };
 
 appData.start();
-
-console.log(appData.fullPrice);
-console.log(appData.servicePercentPrice);
