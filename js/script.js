1388276c66ecd02eaae64d328d8ffded70d13b38
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-11 09:43:52 +0700

    обязательная домашка по 12 уроку

--- js/script.js
+++ js/script.js
@@ -1,101 +1,127 @@
 'use strict'
 
-const titleH1 = document.getElementsByTagName('h1')[0];
-const handlerBtn = document.getElementsByClassName('handler_btn');
-const screenBtn = document.querySelector('.screen-btn');
+const title = document.getElementsByTagName('h1')[0];
+const buttonPlus = document.querySelector('.screen-btn');
 const otherItemsPercent = document.querySelectorAll('.other-items.percent');
 const otherItemsNumber = document.querySelectorAll('.other-items.number');
+
 const inputRange = document.querySelector('.rollback input');
-const spanRange = document.querySelector('.rollback span');
-const totalInput1 = document.getElementsByClassName('total-input')[0];
-const totalInput2 = document.getElementsByClassName('total-input')[1];
-const totalInput3 = document.getElementsByClassName('total-input')[2];
-const totalInput4 = document.getElementsByClassName('total-input')[3];
-const totalInput5 = document.getElementsByClassName('total-input')[4];
+const rangeValue = document.querySelector('.rollback .range-value')
+const inputRangeValue = document.querySelector('.rollback .range-value');
 
-let screen = document.querySelectorAll('.screen');
 
-console.log(screen);
+const startBtn = document.getElementsByClassName('handler_btn')[0];
+const resetBtn = document.getElementsByClassName('handler_btn')[1];
+
+
+const total = document.getElementsByClassName('total-input')[0];
+const totalCount = document.getElementsByClassName('total-input')[1];
+const totalCountOther = document.getElementsByClassName('total-input')[2];
+const fullTotalCount = document.getElementsByClassName('total-input')[3];
+let totalCountRollback = document.getElementsByClassName('total-input')[4];
+
+let screens = document.querySelectorAll('.screen');
+
 
 const appData = {
    title: '',
    screens: [],
    screenPrice: 0,
    adaptive: true,
-   rollback: 10,
-   allServicePrices: 0,
+   rollback: 0,
+   servicePricesPercent: 0,
+   servicePricesNumber: 0,
    fullPrice: 0,
    servicePercentPrice: 0,
-   services: {},
+   servicesPercent: {},
+   servicesNumber: {},
+   init: function () {
+      appData.addTitle();
+
+
+
+      startBtn.addEventListener('click', appData.start)
+      buttonPlus.addEventListener('click', appData.addScreenBlock)
+      inputRange.addEventListener('input', appData.inputRange)
+   },
+   addTitle: function () {
+      document.title = title.textContent;
+   },
    start: function () {
-      appData.asking();
+      appData.addScreens()
+      appData.addServices()
+
       appData.addPrices();
-      appData.getFullPrice();
-      appData.getServicePercentPrices();
-      appData.getTitle();
-      appData.logger();
+
+      // appData.getServicePercentPrices();
+
+      // appData.logger();
+
+      appData.showResult()
    },
-   isNumber: function (num) {
-      return !isNaN(parseFloat(num)) && isFinite(num) // лучшая проверка на число. Проверяет введено ли не действительное число, если да то возвращает true.
+   showResult: function () {
+      total.value = appData.screenPrice;
+      totalCountOther.value = appData.servicePricesPercent + appData.servicePricesNumber;
+      fullTotalCount.value = appData.fullPrice;
    },
-   asking: function () {
+   addScreens: function () {
+      screens = document.querySelectorAll('.screen');
 
-      appData.title = prompt('Как называется ваш проект?', 'Калькулятор вёрстки');
+      screens.forEach(function (screen, index) {
+         const select = screen.querySelector('select');
+         const input = screen.querySelector('input');
+         const selectName = select.options[select.selectedIndex].textContent;
 
-      //проверка, на строку.
-      if (!isNaN(appData.title)) {
-         alert('Необходимо ввести строчное значение');
-         appData.asking();
-      }
-
-      function circleScreens() {
-         for (let i = 0; i < 2; i++) {
-            let name = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные ')
-            let price = 0
-
-            //проверка на строку
-            if (!isNaN(name)) {
-               alert('Необходимо ввести строчное значение');
-               circle()
-            }
-
-            // + делает проверку на число
-            do {
-               price = +prompt('Сколько будет стоить данная работа ?');
-            } while (!appData.isNumber(price))
-
-            appData.screens.push({
-               id: i,
-               name: name,
-               price: price
-            })
+         if (select.value === "" || input.value === "") {
+            startBtn.Enabled = false;
+            alert('Выберите тип экрана и укажите их количество')
+         } else {
+            startBtn.Enabled = true;
          }
-      }
-      circleScreens();
 
-      function circleService() {
-         for (let i = 0; i < 2; i++) {
-            let name = prompt('Какой дополнительный тип услуги нужен?');
-            let price = 0;
+         appData.screens.push({
+            id: index,
+            name: selectName,
+            price: +select.value * +input.value,
+            count: +input.value
+         });
+         console.log(appData.screens);
+      })
+   },
+   // даёт объекту services необходимую информацию
+   addServices: function () {
+      otherItemsPercent.forEach(function (item) {
+         const check = item.querySelector('input[type=checkbox]');
+         const label = item.querySelector('label');
+         const input = item.querySelector('input[type=text]');
 
-            //проверка на строку
-            if (!isNaN(name)) {
-               alert('Необходимо ввести строчное значение');
-               circleService();
-            };
-
-            do {
-               price = prompt('Сколько это будет стоить?');
-            }
-            while (!appData.isNumber(price));
-
-            appData.services[name + i] = +price;
+         if (check.checked) {
+            appData.servicesPercent[label.textContent] = +input.value;
          }
-         console.log(appData.services);
-      }
-      circleService()
 
-      appData.adaptive = confirm('Нужен ли адаптив на сайте?');
+      })
+      otherItemsNumber.forEach(function (item) {
+         const check = item.querySelector('input[type=checkbox]');
+         const label = item.querySelector('label');
+         const input = item.querySelector('input[type=text]');
+
+         if (check.checked) {
+            appData.servicesNumber[label.textContent] = +input.value;
+         }
+
+      })
+
+   },
+   addScreenBlock: function () {
+      const cloneScreen = screens[0].cloneNode(true);
+
+      screens[screens.length - 1].after(cloneScreen)
+   },
+   // Повесить на input[type = range](в блоке с классом.rollback) обработчик события.При перемещении ползунка значение под ним(в элементе span) должно меняться.А так же это значение должно заноситься в свойство rollback нашего объекта для последующих расчетов!
+   inputRange: function () {
+      rangeValue.textContent = inputRange.value + "%";
+      appData.rollback = rangeValue.textContent;
+
    },
 
    addPrices: function () {
@@ -105,43 +131,27 @@ const appData = {
          return a + b.price;
       }, initialValue);
 
-      for (let key in appData.services) {
-         appData.allServicePrices += appData.services[key]
+      for (let key in appData.servicesNumber) {
+         appData.servicePricesNumber += appData.servicesNumber[key]
       };
-   },
 
-   //Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
-   getFullPrice: function () {
-      appData.fullPrice = +appData.screenPrice + appData.allServicePrices
-   },
-
-   // Функция возвращает итоговую стоимость за вычетом процента отката
-   getServicePercentPrices: function () {
-      appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
-   },
-
-   //Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой". Учесть вариант что строка может начинаться с пустых символов. " КаЛьКулятор Верстки"
-   getTitle: function () {
-      appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase();
-   },
-
-   getRollbackMessage: function (price) {
-      if (price >= 30000) {
-         return 'Даем скидку в 10 %';
-      } else if (price >= 15000 && price < 30000) {
-         return 'Даем скидку в 5 %';
-      } else if (price >= 0 && price < 15000) {
-         console.log('Скидка не предусмотрена');
-      } else {
-         console.log('Что то пошло не так');
+      for (let key in appData.servicesPercent) {
+         appData.servicePricesPercent += appData.screenPrice * (appData.servicesPercent[key] / 100)
       };
+      appData.fullPrice = +appData.screenPrice + appData.servicePricesNumber + appData.servicePricesPercent;
+
+      // стоимость с учётом отката
+      totalCountRollback.value = appData.fullPrice - (appData.fullPrice * (parseFloat(appData.rollback) / 100));
+
+
+      // В методе addPrices посчитать общее количество экранов и вывести на страницу итоговое значение в поле с подписью "Количество экранов"
+      totalCount.value = appData.screens.reduce((a, b) => a + b.count, 0);
+      console.log(totalCount.value);
    },
 
    logger: function () {
-      console.log(appData.fullPrice);
-      console.log(appData.servicePercentPrice);
-      console.log(appData.screens);
+
    },
 };
 
-//appData.start();
+appData.init();
